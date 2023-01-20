cf4aa9b4607980833dcc55138df8ddfc8c9889bb
yuan010 <1876218322@qq.com>
2023-01-20 16:15:39 +0800

    three Commit

---
+++ src/main/java/com/yuan/git/Main.java
@@ -0,0 +1,13 @@
+package com.yuan.git;
+
+/**
+ * @author yuanyuan
+ * @version V1.0
+ * @date 2023/1/20 16:14
+ * @Description null
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(11111);
+    }
+}
