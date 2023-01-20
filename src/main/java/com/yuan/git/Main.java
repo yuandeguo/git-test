335c1aeebdf6608a6740c95d98e107156c9bc624
yuan010 <1876218322@qq.com>
2023-01-20 16:24:07 +0800

    four Commit

--- src/main/java/com/yuan/git/Main.java
+++ src/main/java/com/yuan/git/Main.java
@@ -10,5 +10,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(11111);
         System.out.println(22222);
+        System.out.println(33333);
     }
 }
